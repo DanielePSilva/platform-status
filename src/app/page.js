8a1252978eb98f7454947ae9484099b9c38e5634
Daniele Pereira <daniele.intern@c02c73e9lvdm.home>
2024-04-01 12:58:13 +0100

    testing features tailwind css

--- src/app/page.js
+++ src/app/page.js
@@ -3,10 +3,17 @@ import 'tailwindcss/tailwind.css';
 export default function Home() {
   return (
 
-    <div className='bg-cyan-900'>
-      <div>ENGINE AI LOGO</div>
-    
-      <div> cards</div>
+    <div >
+      <div className='text-cyan-800 p-8'>ENGINE AI LOGO</div>
+      <span className='p-16 m-16  space-y-8'> Title</span>
+     <div className=' w-1/4 p-16  m-10  space-y-8 border-2'>
+      <div className='border-b-2  border-l-2'> component 1 <span className=' text-l bg-green-400 rounded-full ml-32' > status</span></div>
+      <div className='border-b-2 border-l-2'> component 2 <span className=' text-l bg-green-400 rounded-full  ml-32' > status</span></div>
+      <div className='border-b-2 border-l-2'> component 3 <span className=' text-l bg-green-400 rounded-full  ml-32' > status</span></div>
+      <div className='border-b-2 border-l-2'> component 4 <span className=' text-l bg-green-400 rounded-full  ml-32' > status</span></div>
+      <div className='border-b-2 border-l-2'> component 5 <span className=' text-l bg-green-400 rounded-full  ml-32' > status</span> </div>
+      </div>
+
     </div>
 
   );
